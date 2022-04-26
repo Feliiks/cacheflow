612482b47ce253420676910f8383692f0d2bf9de
Roman Emmons <6171018-RomanEmmons@users.noreply.gitlab.com>
2022-04-26 02:07:15 +0000

    Feature/athentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import LandingPage from "./components/LandingPage";
-import LoginPage from "./components/LoginPage";
-import RegisterPage from "./components/RegisterPage";
-import ForgetPasswordPage from "./components/ForgetPasswordPage";
-import HomePage from "./components/HomePage";
-
+   
+import { Container, Row, Col } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { UserAuthContextProvider } from "./context/UserAuthContext";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function App() {
+const App = () => {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/register" component={RegisterPage} />
-          <Route path="/forget-password" component={ForgetPasswordPage} />
-          <Route path="/home" component={HomePage} />
-        </Switch>
-        {/* <Footer /> */}
-      </div>
-    </Router>
+    <Container style={{ width: "400px" }}>
+      <Row>
+        <Col>
+          <UserAuthContextProvider>
+            <Routes>
+              <Route
+                path="/home"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </UserAuthContextProvider>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("app")
+);
+
+
+export default App;
