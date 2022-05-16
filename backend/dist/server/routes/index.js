409bab3b315ae4bd879ad74bc6528f1aa0339b17
Feliiks <sobrero.ludovic@gmail.com>
2022-05-16 15:52:09 +0200

    Edit: prisma queries

--- backend/dist/server/routes/index.js
+++ backend/dist/server/routes/index.js
@@ -15,49 +15,47 @@ const database_1 = require("../../database");
 const mainRouter = (0, express_1.Router)();
 mainRouter.get("/userCompaniesModels/:userUid", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const userUid = req.params.userUid.toString();
-    const query = `
-		SELECT
-			u.cacheflow_id as user_id,
-			c.cacheflow_id as company_id,
-			m.cacheflow_id as model_id,
-			u.email as user_name,
-			c.company_name as company_name,
-			c.database_name as company_database_name,
-			m.name as model_name,
-			m.ui_name as model_ui_name,
-			m.upload as upload,
-			m.crud as crud,
-			um.read as read_permission,
-			um.write as write_permission
-		
-		FROM
-			user u
-
-		LEFT JOIN
-			user_company uc ON 
-			uc.user_uid = u.uid
-
-		LEFT JOIN
-			company c ON
-			uc.company_cacheflow_id = c.cacheflow_id
-
-		LEFT JOIN
-			user_model um ON
-			um.user_cacheflow_id = u.cacheflow_id
-
-		LEFT JOIN
-			model m ON
-			m.cacheflow_id = um.model_cacheflow_id
-
-		WHERE
-			u.uid = '${userUid}'
-	`;
-    // prisma connexion test
-    const user = yield database_1.prisma.user.findMany({
-        where: {
-            cacheflow_id: 1
-        }
-    });
-    res.send(user[0].email);
+    try {
+        const user = yield database_1.prisma.user.findUnique({
+            where: {
+                uid: userUid
+            },
+            include: {
+                user_company: true,
+                user_model: true
+            }
+        });
+        const company = yield database_1.prisma.company.findFirst({
+            where: {
+                cacheflow_id: user.user_company[0].cacheflow_id
+            }
+        });
+        const model = yield database_1.prisma.model.findFirst({
+            where: {
+                cacheflow_id: user.user_model[0].cacheflow_id
+            }
+        });
+        let result = {
+            user_id: user.cacheflow_id,
+            company_id: company.cacheflow_id,
+            model_id: model.cacheflow_id,
+            user_name: user.email,
+            company_name: company.company_name,
+            company_database_name: company.database_name,
+            model_name: model.name,
+            model_ui_name: model.ui_name,
+            upload: model.upload,
+            crud: model.crud,
+            read_permission: model.read,
+            write_permission: model.write
+        };
+        res
+            .send(result)
+            .status(200);
+    }
+    catch (err) {
+        console.log(err);
+        res.sendStatus(400);
+    }
 }));
 exports.default = mainRouter;
--- backend/src/server/routes/index.ts
+++ backend/src/server/routes/index.ts
@@ -7,52 +7,52 @@ const mainRouter: Router = Router()
 mainRouter.get("/userCompaniesModels/:userUid", async (req, res) => {
   const userUid = req.params.userUid.toString();
 
-  const query = `
-		SELECT
-			u.cacheflow_id as user_id,
-			c.cacheflow_id as company_id,
-			m.cacheflow_id as model_id,
-			u.email as user_name,
-			c.company_name as company_name,
-			c.database_name as company_database_name,
-			m.name as model_name,
-			m.ui_name as model_ui_name,
-			m.upload as upload,
-			m.crud as crud,
-			um.read as read_permission,
-			um.write as write_permission
-		
-		FROM
-			user u
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        uid: userUid
+      },
+      include: {
+        user_company: true,
+        user_model: true
+      }
+    })
 
-		LEFT JOIN
-			user_company uc ON 
-			uc.user_uid = u.uid
+    const company = await prisma.company.findFirst({
+      where: {
+        cacheflow_id: user!.user_company[0].cacheflow_id
+      }
+    })
 
-		LEFT JOIN
-			company c ON
-			uc.company_cacheflow_id = c.cacheflow_id
+    const model = await prisma.model.findFirst({
+      where: {
+        cacheflow_id: user!.user_model[0].cacheflow_id
+      }
+    })
 
-		LEFT JOIN
-			user_model um ON
-			um.user_cacheflow_id = u.cacheflow_id
-
-		LEFT JOIN
-			model m ON
-			m.cacheflow_id = um.model_cacheflow_id
-
-		WHERE
-			u.uid = '${userUid}'
-	`;
-
-  // prisma connexion test
-  const user = await prisma.user.findMany({
-    where: {
-      cacheflow_id: 1
+    let result = {
+      user_id: user!.cacheflow_id,
+      company_id: company!.cacheflow_id,
+      model_id: model!.cacheflow_id,
+      user_name: user!.email,
+      company_name: company!.company_name,
+      company_database_name: company!.database_name,
+      model_name: model!.name,
+      model_ui_name:  model!.ui_name,
+      upload:  model!.upload,
+      crud:  model!.crud,
+      read_permission:  model!.read,
+      write_permission:  model!.write
     }
-  })
 
-  res.send(user[0].email)
+    res
+        .send(result)
+        .status(200)
+
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(400)
+  }
 })
 
 export default mainRouter
