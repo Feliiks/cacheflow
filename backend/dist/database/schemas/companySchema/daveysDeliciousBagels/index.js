d55cb02a955872390e33594b161c73092f1334f2
Feliiks <sobrero.ludovic@gmail.com>
2022-05-19 15:39:37 +0200

    Edit: untracked files

--- backend/dist/database/schemas/companySchema/daveysDeliciousBagels/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.daveys_delicious_bagels_db = void 0;
-const client_1 = require("@prisma/client");
-const daveys_delicious_bagels_db = new client_1.PrismaClient();
-exports.daveys_delicious_bagels_db = daveys_delicious_bagels_db;
--- backend/dist/database/schemas/platformSchema/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.platform_db = void 0;
-const client_1 = require("@prisma/client");
-const platform_db = new client_1.PrismaClient();
-exports.platform_db = platform_db;
