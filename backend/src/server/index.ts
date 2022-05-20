4b4334c30f2f194f34c3688f8a06df99233d8602
Feliiks <sobrero.ludovic@gmail.com>
2022-05-20 09:53:14 +0200

    Add/edit: index.js file, static files

--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -4,6 +4,7 @@ import { dbs } from "../database";
 const port = 3000;
 import cors from "cors"
 import mainRouter from "./routes";
+import path from "path";
 
 const app = express();
 
@@ -25,10 +26,14 @@ app.use(
   express.urlencoded({ limit: "500mb", extended: true, parameterLimit: 50000 })
 );
 app.use(express.json({ limit: "500mb" }));
-app.use(express.static("../../frontend/public"));
+app.use(express.static("../../../../frontend/public"));
 
 app.use("/platform", mainRouter);
 
 module.exports = app.listen(port, () =>
   console.log(`listening on port ${port}!`)
 );
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../../../frontend/public/index.html"))
+})
